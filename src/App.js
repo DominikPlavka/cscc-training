ccf1da80b09f5d49d887ca5c22b3e1c30045d101
Dominik Plavka <domino.plavka@gmail.com>
2023-05-19 12:31:47 +0200

    Created basic responsive layout usin scss brakpoints document

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="main-wrapper">
+      <h1>TESTING RESPONSIVE SCSS</h1>
+      <div className="testing-layer">
+        <div><p>Content 1</p></div>
+        <div><p>Content 2</p></div>
+        <div><p>Content 3</p></div>
+        <div><p>Content 4</p></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
